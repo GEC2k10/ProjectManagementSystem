6c9ee0170debabf21371a062b52794efad4b17c2
Rajeev S <rajeevs1992@gmail.com>
2011-09-21 00:07:43 +0530

    Created a basic layout of homepage using the GET method and a 40 char long session ID dynamically generated at login.php.And redirected to homepage passin the session id,which is recieved at home page and looked for in the DB.

--- controllers/login.php
+++ controllers/login.php
@@ -15,10 +15,25 @@ if($row['uname']=="")
 }
 else 
 {
-	$ID="hell";
+	$ID='';
+	$caps=array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+	$small=array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+	$num=array('0','1','2','3','4','5','6','7','8','9');
+	for($i=0;$i<40;$i++)
+	{
+	$list=rand(1,3);
+	if($list==1)
+		$ID=$ID.$caps[rand(0,25)];
+	elseif($list==2)
+	 	$ID=$ID.$small[rand(0,25)];
+	else
+	        $ID=$ID.$num[rand(0,9)];
+	}
+	echo $ID;
 	$query="update Accounts set loginStatus='1',sessionID='".$ID."' where uname='".$row['uname']."'";
-	mysql_query($query,$con);
+ 	mysql_query($query,$con);
 	mysql_close($con);
+	header("Location:homePage.php?ssid=$ID");
 }
 ?>
 </body>
