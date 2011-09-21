4d988d5c59ad5c281826d8b65d4faa7d29631e01
PaulDaviesC <pauldaviesc@gmail.com>
2011-09-21 12:02:53 +0530

    Edited login.php

--- controllers/login.php
+++ controllers/login.php
@@ -15,21 +15,11 @@ if($row['uname']=="")
 }
 else 
 {
-	$ID='';
-	$caps=array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-	$small=array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-	$num=array('0','1','2','3','4','5','6','7','8','9');
 	for($i=0;$i<40;$i++)
-	{
-	$list=rand(1,3);
-	if($list==1)
-		$ID=$ID.$caps[rand(0,25)];
-	elseif($list==2)
-	 	$ID=$ID.$small[rand(0,25)];
-	else
-	        $ID=$ID.$num[rand(0,9)];
-	}
-	echo $ID;
+        	if($i%2==0)
+                	 $ID=$ID.chr(rand(97,123));
+	        else if($i%3)
+        	        $ID=$ID.chr(rand(48,57));
 	$query="update Accounts set loginStatus='1',sessionID='".$ID."' where uname='".$row['uname']."'";
  	mysql_query($query,$con);
 	mysql_close($con);
