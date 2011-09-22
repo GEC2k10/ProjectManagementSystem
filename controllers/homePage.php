591e079859525ba540759d51b466171c0cf2fe7e
Rajeev S <rajeevs1992@gmail.com>
2011-09-22 19:53:00 +0530

    Home page on Successful login-Validation done via session id

--- controllers/homePage.php
+++ controllers/homePage.php
@@ -1,14 +1,25 @@
 <html>
-<body style="background-image: url(../views/images.png); color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b">
 <?php
+	session_start();
 	$con=mysql_connect("localhost",'root','root-user');
 	mysql_select_db("GitRepoForPHP");
-	$query = sprintf("SELECT * FROM Accounts WHERE sessionID='".$_GET['ssid']."'");
+	$query = sprintf("SELECT * FROM Accounts WHERE uname='".$_SESSION['username']."'");
 	$reply=mysql_query($query,$con);
+	if (mysql_num_rows($reply)==0)
+	{
+		echo '<body style="background-image: url(../views/images.png); color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b">';
+		echo "<h3>You are not logged in..Please Login...</h3>";
+		echo "<br><a href='../views/login.html'><input type='submit' value='Login'></a>";
+		exit;
+	}
 	$row=mysql_fetch_assoc($reply);
-	echo $row['uname'];
+	mysql_close($con);
 ?>
-
+<body style="background-image: url(../views/images.png); color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b">
+<font face='Ubuntu'>
+<left>Welcome <a href='profiles.php'><b><i><?php echo $row['uname'];?></a></b></i></left>
+<a href='logout.php'><right>Logout</right></a><br>
+<left><font size="4">Project name :</font><?php echo " ".$row['projectName']; ?></left>
+</font>
 </body>
 </html>
-
