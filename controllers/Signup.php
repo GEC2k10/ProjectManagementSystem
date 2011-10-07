2db150e5d291f30c3a06513c861485ef22d98035
PaulDaviesC <pauldaviesc@gmail.com>
2011-10-07 18:21:12 +0530

    Updated

--- controllers/Signup.php
+++ controllers/Signup.php
@@ -28,7 +28,7 @@
 		$len=strlen($_POST['securityQ']);  //Checking whether Security Qn has been entered or not
 		if($len==0){ die ("Please enter a secutiry question"); }
 
-	        $len=strlen($_POST['password_old']);
+	        $len=strlen($_POST['password_new']);
 		if($len<8){echo "Password too short";exit;} //Password length check
 		$sha_old=sha1($_POST['password_old']);
 		$query="SELECT uname,email,passwd FROM Accounts WHERE uname='$_POST[idnum]'";
@@ -41,7 +41,7 @@
 		else 
 		{
         		if(!($row['email']==$_POST['email'])){ die("Email not matching");}
-		        if(!($row['passwd']==$sha_new)){die("Incorrect password");}
+		        if(!($row['passwd']==$sha_old)){die("Incorrect password");}
         		$query="UPDATE Accounts SET  passwd='$sha_new' WHERE uname='$_POST[idnum]'";
 	        	mysql_query($query,$con);
 	        	$query="UPDATE Accounts SET secuQ='$_POST[securityQ]' WHERE uname='$_POST[idnum]'";
