338a7031d699d18acd38c5a883cd746926d7dc39
Rajeev S <rajeevs1992@gmail.com>
2011-10-08 12:31:19 +0530

    Documented

--- controllers/logout.php
+++ controllers/logout.php
@@ -1,11 +1,8 @@
 <html>
 <?php
-	$con=mysql_connect("localhost","root","password");
-	mysql_select_db("GitRepo");
-	$query="UPDATE Accounts SET sessionID=\"0\" where uname='$row[uname]'";
 	session_start();
-	$_SESSION['username']=0;
-	$_SESSION['SessionID']=0;
+	session_destroy();
+	//redirect to login page
 	header('location:../views/login.html');
 ?>
 </html>
--- python/generate.py
+++ python/generate.py
@@ -1,17 +1,19 @@
 import MySQLdb
 import os
+import randPass
 db = MySQLdb.connect(host="localhost",user="root",passwd="password",db="GitRepo")
 cursor = db.cursor()
 projectList=''
 fp=open("data.txt","w")
 n=int(raw_input("Enter number of entries "))
+sessionID=randPass.gen()
 for i in range(0,n):
 	admno=raw_input("Enter admission number of user "+str(i+1)+" ")
 	project=raw_input("Enter project name of user "+str(i+1)+" ")
 	if project not in projectList:
 		projectList+=project+" "
 	email=raw_input("Enter email ID of user "+str(i+1)+" ")
-	data=admno+"| |"+project+"|"+email+"| | |0|0|0\n"
+	data=admno+"| |"+project+"|"+email+"| | |0|"+sessionID+"|0\n"
 	fp.write(data)
 fp.close()
 projectList=projectList.split()
