a198be893901d2cc95d92a8fc868dd42e08fb285
Vijeenrosh P.W <hsorhteeniv@gmail.com>
2011-10-30 02:46:35 -0400

    Login page made Neet

--- controllers/login.php
+++ controllers/login.php
@@ -2,8 +2,8 @@
 session_start();
 ?>
 <?php
-require_once("login.class.php");
-$user=new Login($_POST["uname"],$_POST["passwd"]);
-$user->Authenticate();
+require_once("../classes/login.class.php");
+$user=new Login($_POST["uname"],$_POST["passwd"]);  // create an user object 
+$user->Authenticate();  // Authentication 
 ?>
 
