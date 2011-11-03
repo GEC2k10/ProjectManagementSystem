7cd661e9fcca069b54634d74010be9ec10ccaf9f
PaulDaviesC <pauldaviesc@gmail.com>
2011-11-03 15:25:32 +0530

    Design change : Student & Guide DB are the same

---
+++ python/UpdateDB.py
@@ -0,0 +1,6 @@
+#This script will update th DB according to the files dataguide.txt and  datauser.txt
+import MySQLdb
+db = MySQLdb.connect(host="localhost",user="root",passwd="password",db="GitRepo")
+cursor = db.cursor()
+cursor.execute("load data local infile \"/var/www/lag/python/datauser.txt\" into table Accounts fields terminated by \"|\"")
+cursor.execute("load data local infile \"/var/www/lag/python/dataguide.txt\" into table Accounts fields terminated by \"|\"")
