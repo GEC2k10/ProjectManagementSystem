aaeb35f81af00b81cf30ccc0a65e5c14dd3a2d6a
Rajeev S <rajeevs1992@gmail.com>
2011-11-13 13:19:42 +0530

    Added a success message on upload

--- controllers/gitCommands/upload.php
+++ controllers/gitCommands/upload.php
@@ -1,13 +1,14 @@
 <html>
-<title>Upload Files to the central Repository</title>
 <?php
 	session_start();
 	$target="/var/www/lag/controllers/gitCommands/repos/".$_SESSION['project']."/";
 	$_SESSION['message']=' ';
 	$target=$_POST['directory'].'/';
-	echo $target;
 	$target=$target.basename($_FILES['file']['name']);
-	move_uploaded_file($_FILES['file']['tmp_name'],$target);
+	if(move_uploaded_file($_FILES['file']['tmp_name'],$target))
+	{
+		$_SESSION['message']="Successfully` uploaded file".$_FILES['file']['name'];
+	}
 	header("location:../../views/upload.php");
 ?>
 </html>
