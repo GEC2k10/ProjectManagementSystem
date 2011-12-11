f5542c93c6f14b8aa81b4d809e60bd076966fbc8
Rajeev S <rajeevs1992@gmail.com>
2011-12-11 14:45:14 +0530

    trivial

--- controllers/homePage.php
+++ controllers/homePage.php
@@ -1,49 +1,49 @@
 <html>
 <head>
-     <link rel="shortcut icon" href="../views/home.png">
+<link rel="shortcut icon" href="../views/home.png">
 </head>
 <?php
 /****************************************************************************************
 homePage.php is the homepage of a current user.Contains the buttons to perform git actions.
 ***************************************************************************************/
-	session_start();
-	$con=mysql_connect("localhost",'root','password');
-	mysql_select_db("GitRepo");
-	$query = "SELECT * FROM Accounts WHERE sessionID='".$_SESSION['SessionID']."'";
-	//Selects row that matches sessionId of current session.The session ID was written into the database in the login page.
-	$reply=mysql_query($query,$con);
-	if (mysql_num_rows($reply)==0)
-	{
-		//User not logged in and tries to access hompepage.php by typing in the url
-		echo '<body style="background-image: url(../views/images.png); color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b">';
-		echo "<h3>You are not logged in..Please Login...</h3>";
-		echo "<br><a href='../views/login.html'><input type='submit' value='Login'></a>";
-		exit;
-	}
-	$row=mysql_fetch_assoc($reply);
-	echo "<title>Homepage of ".$row['uname']."</title>";
-	$_SESSION['project']=$row['projectName'];
-	if($row['activationStatus']=='0')
-		header("location:../views/activate.html");
-		//This may seem to be pointless here as it is a repeatation of script in login page.But this is necessary to avoid the user from acessing the homepage by bypassing the activation process 
-	mysql_close($con);
+session_start();
+$con=mysql_connect("localhost",'root','password');
+mysql_select_db("GitRepo");
+$query = "SELECT * FROM Accounts WHERE sessionID='".$_SESSION['SessionID']."'";
+//Selects row that matches sessionId of current session.The session ID was written into the database in the login page.
+$reply=mysql_query($query,$con);
+if (mysql_num_rows($reply)==0)
+{
+//User not logged in and tries to access hompepage.php by typing in the url
+echo '<body style="background-image: url(../views/images.png); color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b">';
+echo "<h3>You are not logged in..Please Login...</h3>";
+echo "<br><a href='../views/login.html'><input type='submit' value='Login'></a>";
+exit;
+}
+$row=mysql_fetch_assoc($reply);
+echo "<title>Homepage of ".$row['uname']."</title>";
+$_SESSION['project']=$row['projectName'];
+if($row['activationStatus']=='0')
+header("location:../views/activate.html");
+//This may seem to be pointless here as it is a repeatation of script in login page.But this is necessary to avoid the user from acessing the homepage by bypassing the activation process
+mysql_close($con);
 ?>
 <?php echo $_SESSION['message'];$_SESSION['message']=''; ?>
 <body style="background-image: url(../views/images.png); color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b">
 <font face='Ubuntu'>
 <h1><center><font color=white> Welcome <b><?php echo $row['uname'];?></a></b></font></center></h1>
 <form method='post' action='logout.php' align='right'>
-        <input type='submit' value='Logout'>
+<input type='submit' value='Logout'>
 </form>
 <font size="6" color="white">Project name :<?php echo " ".$row['projectName']; ?></font><br><br>
 <a href='../views/upload.php'><input type='submit' value='Upload a file'></a><br><br>
 </form>
 <form method='post' action='gitCommands/download.php'>
-        <input type='submit' value='Download all files'>
+<input type='submit' value='Download all files'>
 </form>
 <form method='post' action='../views/mkdir.php'>
-        <input type='submit' value='New Directory'>
+<input type='submit' value='New Directory'>
 </form>
-</font> 
+</font>
 </body>
 </html>
