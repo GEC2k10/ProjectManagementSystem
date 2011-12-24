519da75bbf68551628b9102b75fcea75ac815bbc
Rajeev S <rajeevs1992@gmail.com>
2011-12-24 11:40:31 +0530

    edited login page dimensions

--- controllers/gitCommands/download.php
+++ controllers/gitCommands/download.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-exec("tar -cf /var/www/downloads/download.tar /var/www/repos/$_SESSION[project]");
+exec("tar -cf /var/www/downloads/$_SESSION[SessionID].tar /var/www/repos/$_SESSION[project]");
 header( 'Content-type: application/x-gzip-compressed' );    // sets content type
 header( 'Content-Disposition: attachment; filename="/var/www/downloads/download.tar" '); // replace filenamemask  with name with which you  allowed to download the file      
 header( 'Content-transfer-encoding: binary' );        //sets encoding
