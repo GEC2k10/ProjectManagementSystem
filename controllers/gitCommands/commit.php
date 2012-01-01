e3f2721d757525698f23759ef7941942aa032ead
Rajeev S <rajeevs1992@gmail.com>
2012-01-01 23:56:27 +0530

    Added the commit head to the table on commit.

--- controllers/gitCommands/commit.php
+++ controllers/gitCommands/commit.php
@@ -1,11 +1,19 @@
 <?php
+$con=mysql_connect("localhost","root","password");
+mysql_select_db("GitRepo");
 $path = "/var/www/repos/$_POST[projectName]";  // project name is passed to this script
 chdir($path);
 exec("git add * ");
 exec("git commit -am '$_POST[commitMessage]'",$a); // commits with an message 'message' 
-echo "Commit summary<br>";
-foreach ($a as $tmp)
-	echo $tmp."<br>";
-echo "<h3 align = center> Succesfully commited all the files.</h3>";
+if($a[0][0]=='[')
+{
+	echo "<h1>Commited Succesfully!!!!</h1>";
+	$head=substr($a[0],8,7);
+	$query="INSERT INTO Contributions VALUES('$_POST[projectName]','$head',NOW())";
+	mysql_query($query,$con);
+}
+else
+	echo "<h1>Nothing to Commit!!!</h1>";
+mysql_close();
 ?>
 
