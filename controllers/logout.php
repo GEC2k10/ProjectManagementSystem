69df735900af7620d18428ea36447d052a75fcde
Rajeev S <rajeevs1992@gmail.com>
2012-01-03 05:50:19 +0530

    Removed the necessity of database for logout by using session_unset() along with session_destroy().Thanks to stackoverflow

--- controllers/logout.php
+++ controllers/logout.php
@@ -1,12 +1,8 @@
 <html>
 <?php
 	session_start();
-	$con=mysql_connect("localhost",'root','password');
-	mysql_select_db("GitRepo");
-	$query = "UPDATE Accounts SET sessionID='0' WHERE sessionID='$_SESSION[SessionID]'";
-	$reply=mysql_query($query,$con);
+	session_unset();
 	session_destroy();
-	mysql_close();
 	//redirect to login page
 	header('location:../views/login.html');
 ?>
