9a13304d69e3102cb63aa2276ae3f5dca573b28c
PaulDaviesC <pauldaviesc@gmail.com>
2012-01-19 17:25:56 +0530

    views/showfil.php improved

--- controllers/gitCommands/download.php
+++ controllers/gitCommands/download.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
-exec("tar -cf /var/www/downloads/$_SESSION[SessionID].tar /var/www/repos/$_SESSION[project]");
-header( 'Content-type: application/x-gzip-compressed' );    // sets content type
-header( 'Content-Disposition: attachment; filename="/var/www/downloads/download.tar" '); // replace filenamemask  with name with which you  allowed to download the file      
+//exec("tar -zcf /var/www/downloads/$_SESSION[SessionID].tgz /var/www/repos/$_SESSION[project]");
+exec("tar -zcf /var/www/downloads/download.tgz /var/www/repos/$_SESSION[project]");
+header( 'Content-Type: application/octet-stream' );    // sets content type
+header( 'Content-Disposition: attachment; filename="/var/www/downloads/download.tgz" '); // replace filenamemask  with name with which you  allowed to download the file      
 header( 'Content-transfer-encoding: binary' );        //sets encoding
 header( 'Content-Length: '.filesize("filename") );    // replace filename with file yu want to alow to download
-readfile( "filename" );  // repeat the same here
+readfile("filename" );  // repeat the same here
 ?>
 
--- views/showfile.php
+++ views/showfile.php
@@ -1,14 +1,26 @@
 <html>
+<style type="text/css">
+body {
+	background-color:#cfcfcf;
+} 
+</style>
 <head></head>
 <body>
 <?php
+session_start();
+if (!isset($_SESSION['projectName']) ) {   //Security Check
+header("Location:../views/login.html");
+}
 require_once("../classes/showfile.class.php");
-echo "<h3>File :".$_GET['filename']."</h3>";
+echo "<br/><h3>File :".$_GET['filename']."</h3>";
 $file = new ShowFile("/var/www/repos/".$_GET['filename']);
 $file->show_file();
 $file->show_contribution_dates("/var/www/repos/".$_GET['filename'],$_GET['uname']);
-?>
+?> 
+<form method=post action=../controllers/homePage.php >
+       <div style='top:1px;left:1200px;position:absolute'>
+       <input type=submit value=Home>
+       </div>
+</form>
 </body>
 </html>
-
-
