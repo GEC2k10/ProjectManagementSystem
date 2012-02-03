05d8eb2433fd6097ab97d0a62fe1b4c55653fc37
Rajeev S <rajeevs1992@gmail.com>
2012-02-03 22:07:59 +0530

    init at every commit

--- controllers/gitCommands/commit.php
+++ controllers/gitCommands/commit.php
@@ -3,6 +3,7 @@ $con=mysql_connect("localhost","root","password");
 mysql_select_db("GitRepo");
 $path = "/var/www/repos/$_POST[projectName]";  // project name is passed to this script
 chdir($path);
+exec("git init ");
 exec("git add * ");
 exec("git commit -am '$_POST[commitMessage]'",$a); // commits with an message 'message' 
 if($a[0][0]=='[')
