6dfaf5b34df9f92e127655e3ae4c419ac258bd7c
Rajeev S <rajeevs1992@gmail.com>
2012-02-13 22:34:06 +0530

    deleted files

--- controllers/GuideHome.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("../classes/guide.class.php");
-$guide=new Guide("Ruby");
-$guide->show_commit_button();
-?>
--- test.php
+++
@@ -1,4 +0,0 @@
-<?
-	$str="hello gehwj kjjf nn skksks %s ";
-	echo sprintf($str,"hello");
-?>
